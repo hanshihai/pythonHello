dde66f775bcdb48595a9be3f3b5692f76f376d0a
hans <shi-hai.han@dxc.com>
2019-01-02 16:47:02 +0800

    enable special tax discard calculator

--- tax.py
+++ tax.py
@@ -5,6 +5,8 @@ version : v0.1
 
 '''
 
+import TaxRating
+
 COMPANY_AGE = 0.20
 COMPANY_MEDICAL = 0.095
 COMPANY_JOB = 0.005
@@ -19,15 +21,15 @@ PERSONAL_JOB = 0.005
 PERSONAL_HOUSE = 0.07
 PERSONAL_ADDITIONAL_HOUSE = 0.03
 
+
 def main():
 
-    total_input = input("Please input the base month income, and the rating money with space split and tzx, such as 12000 12000 500: ")
+    total_input = input("Please input the base month income, and the rating money with space split and tax special discard money, such as 12000 12000 2000: ")
     input_list = total_input.split(' ')
     total_income = input_list[0]
     rating_money = input_list[1]
 
-    #TODO need to know how to calculate tax
-    tax_money = input_list[2]
+    tax_special_discard_money = input_list[2]
 
     company_age_output = float(rating_money) * COMPANY_AGE
     company_medical_output = float(rating_money) * COMPANY_MEDICAL
@@ -51,14 +53,16 @@ def main():
     company_format_output = "The money that company provided:  age {},     medical {},    job {},     house {},   additional_house {},    insurance_hurting {},   insurance_baby {},  and total {}"
     personal_format_output = "The money that personal provided: age {},     medical {},     job {},     total house {} ({} + {}),    and total {}"
 
+    print('')
     print(company_format_output.format(company_age_output, company_medical_output, company_job_output, company_house_output, company_additional_house_output, company_insurance_hurting, company_insurance_baby, company_total_output))
     print('')
     print(personal_format_output.format(personal_age_output, personal_medical_output, personal_job_output, personal_house_total, personal_house_output, personal_additional_house_output, personal_total_output))
     print('')
 
     personal_income_with_tax = float(total_income) - personal_total_output
-    personal_income_without_tax = personal_income_with_tax - float(tax_money)
-    print("the personal total income {},    income with tax {},     tas {},     income without tax {}".format(total_income, personal_income_with_tax, tax_money, personal_income_without_tax))
+    tax_money = TaxRating.calculateTax(personal_income_with_tax, (float)(tax_special_discard_money))
+    personal_income_without_tax = personal_income_with_tax - tax_money
+    print("the personal total income {},    income with tax {},     tax {},     income without tax {}".format(total_income, personal_income_with_tax, tax_money, personal_income_without_tax))
 
 
 if __name__ == '__main__':
